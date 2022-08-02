74693cbfc6fffca582c4d4c36d4f8c10982fc1f7
URK21CS2002 <109648340+URK21CS2002@users.noreply.github.com>
2022-08-02 23:49:52 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,45 @@
-def evalPostfix(text):
-    s = Stack()
+class Evaluate:
+
+    def __init__(self, Z):
+        self.top = -1
+        self.size_of_stack = Z
+        self.stack = []
+
+    def is_Empty(self):
+        if self.top == -1:
+            return True
+        else: 
+            return False
+
+
+    def pop(self):
+        if not self.isEmpty():
+            self.stack.pop()
+
+
+    def push(self, oprd):
+        if self.top != self.size_of_stack - 1:
+             self.stack.append(oprd)
+
+    def validate_postfix_expression(self, exp):
+
+        v = 0
+        w = 0
+        for ele in exp:
+            if ele.isnumeric():
+                v = v + 1
+            else:
+                w = w + 1
+         if w == v - 1:
+                 return True
+         else:
+                return False
+
+
+     def evalPostfix(text):
+        s = Stack()
     for symbol in text:
-        if symbol in "0123456789":
+        if symbol in text:
             s.push(int(symbol))
         if not s.is_empty():
             if symbol == "+":
@@ -14,11 +52,12 @@ def evalPostfix(text):
                 plus = s.pop() / s.pop()
 
 
+
 # Do not change the following code
-postfix_expression = input()  # Read postfix expression
-tokens = postfix_expression.split()
+postfix = input()  # Read postfix expression
+tokens = postfix.split()
 evaluate = Evaluate(len(tokens))
 if evaluate.validate_postfix_expression(tokens):
-    print(evaluate.evaluate_postfix_expression(tokens))
+    print(evaluate.postfix(tokens))
 else:
     print('Invalid postfix expression')
